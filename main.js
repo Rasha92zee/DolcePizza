6b0555e34db82867cf7b950ed7294f198e75a474
Rasha92zee <rashahasoon923@gmail.com>
2025-01-25 21:09:51 +0530

    About page

---
+++ main.js
@@ -0,0 +1,13 @@
+// SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    // reset: true, // Animations repeat
+  });
+
+  sr.reveal('.about__data')
+  sr.reveal('.about__board', { delay: 700, distance: '100px', origin: 'right' })
+  sr.reveal('.about__pizza', { delay: 1400, distance: '100px', origin: 'bottom', rotate: { z: -90 } })
+  sr.reveal('.about__ingredient', { delay: 2000, interval: 100 })
