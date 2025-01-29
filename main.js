58ff8d7316b1308b0d32f9bf1949584950ae9081
Rasha92zee <rashahasoon923@gmail.com>
2025-01-29 12:41:30 +0530

    Service Section

--- main.js
+++ main.js
@@ -16,7 +16,12 @@ const sr = ScrollReveal({
     // reset: true, // Animations repeat
   });
 
-  sr.reveal('.about__data')
+  sr.reveal('.about__data , .menu__container')
   sr.reveal('.about__board', { delay: 700, distance: '100px', origin: 'right' })
   sr.reveal('.about__pizza', { delay: 1400, distance: '100px', origin: 'bottom', rotate: { z: -90 } })
   sr.reveal('.about__ingredient', { delay: 2000, interval: 100 })
+  sr.reveal('.recipe__list', { origin: 'right' })
+  sr.reveal('.recipe__img', { origin: 'left' })
+  sr.reveal('.service-item', { delay: 700, distance: '40px', origin: 'bottom' });
+
+  
